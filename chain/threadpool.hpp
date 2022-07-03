0e89e737ca46f758f4287fa21c74dc8611002939
Chih-Chen Kao <chihchen.kao@icloud.com>
2022-07-03 21:42:21 +0200

    Add thread pool class. Format timer class.

---
+++ chain/threadpool.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <condition_variable>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+namespace chain {
+
+class threadpool final {
+ public:
+  threadpool();
+
+  /// Enqueue a callable into the thread pool
+  /// @param[in] callable The callable object to be called.
+  /// @param[in] args the arguments of the callable.
+  /// @return The future object of the result of the callable.
+  template <class Callable, class... Args>
+  [[nodiscard]] auto enqueue(Callable &&callable, Args &&...args)
+      -> std::future<typename std::invoke_result<Callable, Args...>::type>;
+
+  ~threadpool();
+
+ private:
+  std::vector<std::thread> workers;
+
+  std::queue<std::packaged_task<void()>> tasks;
+
+  mutable std::mutex queue_mutex;
+  std::condition_variable condition;
+
+  bool stop{false};
+};
+
+threadpool::threadpool() {
+  const auto num_threads{std::thread::hardware_concurrency()};
+
+  for (auto i{0UL}; i < num_threads; ++i) {
+    workers.emplace_back([this]() {
+      while (true) {
+        std::packaged_task<void()> task;
+        {
+          std::unique_lock<std::mutex> lock(queue_mutex);
+          condition.wait(lock, [this] { return stop || !tasks.empty(); });
+
+          if (stop && tasks.empty()) {
+            return;
+          }
+          task = std::move(tasks.front());
+          tasks.pop();
+        }
+
+        std::invoke(task);
+      }
+    });
+  }
+}
+
+template <class Callable, class... Args>
+auto threadpool::enqueue(Callable &&callable, Args &&...args) -> std::future<std::invoke_result_t<Callable, Args...>> {
+  using return_type = std::invoke_result_t<Callable, Args...>;
+
+  std::packaged_task<return_type()> task{
+      [&]() { return std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...); }};
+
+  auto task_future{task.get_future()};
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    tasks.emplace(std::move(task));
+  }
+
+  condition.notify_one();
+  return task_future;
+}
+
+threadpool::~threadpool() {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    stop = true;
+  }
+
+  condition.notify_all();
+  for (auto &worker : workers) {
+    worker.join();
+  }
+}
+
+}  // namespace chain
--- chain/timer.hpp
+++ chain/timer.hpp
@@ -12,28 +12,52 @@ class timer final {
   using duration = std::chrono::duration<Rep>;
 
   template <typename Callable, typename... Args>
-  duration measure(const Token token, Callable&& callable, Args&&... args) noexcept {
-    const auto start{std::chrono::steady_clock::now()};
-    std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
-    const auto stop{std::chrono::steady_clock::now()};
+  duration measure(const Token token, Callable&& callable, Args&&... args) noexcept;
 
-    time_record[token] += (stop - start);
+  /// Get the measured time record of the speficied token.
+  /// @param[in] token The token of the time record.
+  /// @return The corresponding time record of the token.
+  [[nodiscard]] duration get_measurement(const Token token) const noexcept;
 
-    return time_record[token];
-  }
+  /// Reset the time record which corresponds to a specific token.
+  /// @param[in] token The token of the time record.
+  void reset(const Token token) noexcept;
 
-  [[nodiscard]] duration get_measurement(const Token token) const noexcept { return time_record.at(token); }
-
-  void reset(const Token token) noexcept {
-    if (time_record.count(token) > 0UL) {
-      time_record[token] = duration{};
-    }
-  }
-
-  void clear() noexcept { time_record.clear(); }
+  /// Clear all the time record.
+  void clear() noexcept;
 
  private:
   std::unordered_map<Token, duration> time_record;
 };
+
+template <typename Token, typename Rep>
+template <typename Callable, typename... Args>
+typename timer<Token, Rep>::duration timer<Token, Rep>::measure(const Token token, Callable&& callable,
+                                                                Args&&... args) noexcept {
+  const auto start{std::chrono::steady_clock::now()};
+  std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
+  const auto stop{std::chrono::steady_clock::now()};
+
+  time_record[token] += (stop - start);
+
+  return time_record[token];
+}
+
+template <typename Token, typename Rep>
+typename timer<Token, Rep>::duration timer<Token, Rep>::get_measurement(const Token token) const noexcept {
+  return time_record.at(token);
+}
+
+template <typename Token, typename Rep>
+void timer<Token, Rep>::reset(const Token token) noexcept {
+  if (time_record.count(token) > 0UL) {
+    time_record[token] = duration{};
+  }
+}
+
+template <typename Token, typename Rep>
+void timer<Token, Rep>::clear() noexcept {
+  time_record.clear();
+}
 
 }  // namespace chain
